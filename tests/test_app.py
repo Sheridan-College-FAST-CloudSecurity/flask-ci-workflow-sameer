5be36d540bc44af06734abbe0e8c0f141468733e
Sameer shaik <samhash110@gmail.com>
2025-02-05 22:22:18 +0000

    Add CI workflow for Flask application

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import app
+
+def test_hello():
+       response = app.test_client().get('/')
+       assert response.status_code == 200
+       assert response.data == b"Hello, DevSecOps!"
